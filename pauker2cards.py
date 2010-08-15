f1e542ed0b024292ead1b3ca93bea2237992372a
Mark Probst <mark.probst@gmail.com>
2010-08-15 17:27:58 +0200

    Partitioning in the command line tool.

--- pauker2cards.py
+++ pauker2cards.py
@@ -29,10 +29,16 @@ def ident (x):
 
 def main ():
     parser = OptionParser ()
+    parser.add_option ("-p", "--partition", dest = "partition", help = "Partition into sets of that many cards")
     (options, cmdline_args) = parser.parse_args ()
-    if len (cmdline_args) != 1:
-        sys.stderr.write ("Usage: pauker2cards.py <inputfile>\n")
+    if len (cmdline_args) != 2:
+        sys.stderr.write ("Usage: pauker2cards.py <inputfile> <outputfile>\n")
         exit (1)
+
+    if options.partition:
+        partition = int (options.partition)
+    else:
+        partition = None
 
     pauker_file = open (cmdline_args [0], "r")
     pauker_data = pauker_file.read ()
@@ -42,7 +48,18 @@ def main ():
     parser = parsers.PaukerParser (Card, ident, lesson)
     cards = parser.parse (pauker_data)
 
-    writers.write_cards (lesson, cards, sys.stdout.write)
+    if partition:
+        i = 0
+        while len (cards) > 0:
+            out = open (cmdline_args [1] % i, "w")
+            writers.write_cards (lesson, cards [:partition], out.write)
+            out.close ()
+            i = i + 1
+            cards = cards [partition:]
+    else:
+        out = open (cmdline_args [1], "w")
+        writers.write_cards (lesson, cards, out.write)
+        out.close ()
 
 if __name__ == "__main__":
     main()
